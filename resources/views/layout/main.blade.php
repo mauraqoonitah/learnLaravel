42c2032cd8d4173f2c036ad45a37363d2e9c7fda
mauraqp <mauraqoonitah@gmail.com>
2021-02-25 15:21:45 +0700

    controller, database, migration, model

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -14,15 +14,15 @@
     <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#"Learn Laravel</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="/">Home</a>
-        <a class="nav-link" href="/about">About</a>
-        <a class="nav-link" href="/mahasiswa">Mahasiswa</a>
+        <a class="nav-link active" aria-current="page" href="{{ url('/')}}">Home</a>
+        <a class="nav-link" href="{{ url('/about')}}">About</a>
+        <a class="nav-link" href="{{ url('/mahasiswa')}}">Mahasiswa</a>
       </div>
     </div>
   </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\MahasiswaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,13 +15,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
-Route::get('/about', function () {
-    $nama = 'Maura Qoonitah Putri';
-    return view('about', ['nama' => $nama]);
-});
-Route::get('/mahasiswa', function () {
-    return view('mahasiswa');
-});
+// Route::get('/', function () {
+//     return view('index');
+// });
+// Route::get('/about', function () {
+//     $nama = 'Maura Qoonitah Putri';
+//     return view('about', ['nama' => $nama]);
+// });
+// Route::get('/mahasiswa', function () {
+//     return view('mahasiswa');
+// });
+
+// opsi jika tidak pakai use namespace
+// Route::get('/', 'App\Http\Controllers\PagesController@home');
+
+Route::get('/', [PagesController::class, 'home']);
+Route::get('/about', [PagesController::class, 'about']);
+
+Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
