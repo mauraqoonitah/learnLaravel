d7eb0258c5230606ba942c09783c5225998668ad
mauraqp <mauraqoonitah@gmail.com>
2021-02-27 22:30:08 +0700

    add old input form

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -12,7 +12,7 @@
        @csrf
         <div class="form-group mb-3">
           <label for="nama" class="form-label">Nama</label>
-          <input type="text" placeholder="Masukkan nama" class="form-control @error('nama') is-invalid @enderror" id="nama" name="nama">
+          <input type="text" placeholder="Masukkan nama" class="form-control @error('nama') is-invalid @enderror" id="nama" name="nama" value="{{ old('nama') }}">
 
           @error('nama') 
           <div class="invalid-feedback">
@@ -24,7 +24,7 @@
 
         <div class="form-group mb-3">
           <label for="nrm" class="form-label">NRM</label>
-          <input type="text" placeholder="Masukkan nrm" class="form-control @error('nrm') is-invalid @enderror" id="nrm" name="nrm">
+          <input type="text" placeholder="Masukkan nrm" class="form-control @error('nrm') is-invalid @enderror" id="nrm" name="nrm" value="{{ old('nrm') }}">
 
           @error('nrm') 
           <div class="invalid-feedback">
@@ -36,7 +36,7 @@
 
         <div class="form-group mb-3">
           <label for="email" class="form-label">Email</label>
-          <input type="text" placeholder="Masukkan email" class="form-control @error('email') is-invalid @enderror" id="email" name="email">
+          <input type="text" placeholder="Masukkan email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" value="{{ old('email') }}">
 
           @error('email') 
           <div class="invalid-feedback">
@@ -49,7 +49,7 @@
 
         <div class="form-group mb-3">
           <label for="jurusan" class="form-label">Jurusan</label>
-          <input type="text" placeholder="Masukkan jurusan" class="form-control" id="jurusan" name="jurusan">
+          <input type="text" placeholder="Masukkan jurusan" class="form-control" id="jurusan" name="jurusan" value="{{ old('jurusan') }}">
         </div>
       
         <button type="submit" class="btn btn-primary">Tambah</button>
